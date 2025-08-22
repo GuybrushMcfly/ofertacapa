6211638317722f6571722aef7e0b8ec0848d2bc2
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-08-22 08:20:56 -0300

    Update ofertas.py

--- views/ofertas.py
+++ views/ofertas.py
@@ -162,7 +162,7 @@ def mostrar():
                         html += '<span class="no-link">Sin enlace</span>'
                     # 🚀 Nuevo botón Copiar
                     actividad = row["Actividad (Comisión)"]
-                    html += f'<a href="#" onclick="copyToField(`{actividad}`)" class="boton">📋 Copiar</a>'
+                    html += f'<a href="javascript:void(0);" onclick="copyToField(`{actividad}`)" class="boton">📋 Copiar</a>
                     html += '</td>'
                 else:
                     html += f"<td>{val}</td>"
