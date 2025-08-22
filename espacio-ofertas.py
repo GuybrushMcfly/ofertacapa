c52308508b946b6774a5489c5297f15f7b95c89f
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-08-21 21:26:37 -0300

    Update espacio-ofertas.py

--- espacio-ofertas.py
+++ espacio-ofertas.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="Espacio de Ofertas de Capacitación", layout="wide")
 
 # Si llega cambio desde la tabla (botón HTML)
-query_params = st.experimental_get_query_params()
+query_params = st.query_params
 if "selected_tab" in query_params:
     st.session_state.vista_actual = query_params["selected_tab"][0]
     st.query_params  # Limpia la URL
