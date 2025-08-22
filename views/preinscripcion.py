615cde555fce0bd8ea884cf52f1f73e40cdc44c8
GuybrushMcfly <113950867+GuybrushMcfly@users.noreply.github.com>
2025-08-22 11:33:16 -0300

    Update preinscripcion.py

--- views/preinscripcion.py
+++ views/preinscripcion.py
@@ -183,11 +183,11 @@ def mostrar():
 
               # 🔹 Mensajes secuenciales para INAP
             if st.session_state.get("datos_agenteform") and fila["organismo"] == "INAP":
-                st.toast("📌 Seleccionaste una actividad del INAP")
+                st.toast("❗ Seleccionaste una actividad del INAP")
                 time.sleep(2)  # espera 2 segundos
-                st.toast("📝 Recordá que tenés que preinscribirte en el formulario INDEC")
+                st.toast("⚠️ Recordá que tenés que preinscribirte en el formulario INDEC")
                 time.sleep(2)  # espera 2 segundos
-                st.toast("⚠️ Y también tenés que preinscribirte en el INAP.\nSiempre deben hacerse la doble preinscripción")      
+                st.toast("⚠️ Y también tenés que preinscribirte en el INAP.\nSiempre debes completar la doble preinscripción")      
             
             #datos = st.session_state["datos_agenteform"]
             #if datos:
